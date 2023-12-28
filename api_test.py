7aaa4bdd0b4e4d514d98ce74f150a9fb582c331f
Alfa-Tron <45252746+Alfa-Tron@users.noreply.github.com>
2023-12-28 18:21:29 +0500

    Update api_test.py
    j

--- api_test.py
+++ api_test.py
@@ -1,7 +1,7 @@
 from model import get_answer
 
 
-# Тест на получение ответа
+# Тест на получение отета
 def test_get_answer():
     result = get_answer('What are the advantages of using Transformers models?',
                         'Transformers is a family of machine learning models that represent a significant step forward in natural language processing. They provide outstanding performance in tasks such as machine translation, question-answer system, text summarization and more. The advantages of using Transformers models include high accuracy, the ability to learn from large amounts of data, and the ability to apply to various text processing tasks. These models have also become the basis for many state-of-the-art NLP solutions.')
